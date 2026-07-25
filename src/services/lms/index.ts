f46a242bd73ea83846d102298b0851ad2dc88d76
supportaxonaccadmy-creator <supportaxonaccadmy@gmail.com>
2026-07-25 11:00:55 +0530

    Phase 2A.1: LMS service barrel export

---
+++ src/services/lms/index.ts
@@ -0,0 +1,8 @@
+export { batchService } from './batchService';
+export type { BatchListOptions } from './batchService';
+export { subjectService } from './subjectService';
+export type { SubjectListOptions } from './subjectService';
+export { chapterService } from './chapterService';
+export type { ChapterListOptions } from './chapterService';
+export { classService } from './classService';
+export type { ClassListOptions } from './classService';
