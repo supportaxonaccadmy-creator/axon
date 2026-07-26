61b2f39bf80c72daa57e652258c54225ea8ddc02
supportaxonaccadmy-creator <supportaxonaccadmy@gmail.com>
2026-07-26 17:42:55 +0530

    Phase 3A.1: Add student dashboard type re-exports to types barrel

---
+++ src/types/studentDashboardReexport.ts
@@ -0,0 +1,11 @@
+export type { DateRange, DurationParts, RelativeDateResult, ExpiryResult } from './date';
+export type {
+  StudentBatchInfo,
+  StudentContinueLearning,
+  StudentRecentClass,
+  StudentLiveClass,
+  StudentAnnouncement,
+  StudentProgress,
+  StudentQuickAction,
+  StudentDashboardSummary,
+} from './studentDashboard';
