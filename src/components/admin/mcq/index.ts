6ce825e067f4822d056ac01a4ad09ebd4f227fb3
supportaxonaccadmy-creator <supportaxonaccadmy@gmail.com>
2026-07-28 00:03:17 +0530

    Phase 4B.1: MCQ components barrel exports

---
+++ src/components/admin/mcq/index.ts
@@ -0,0 +1,6 @@
+export { McqStatistics } from './McqStatistics';
+export { McqQuestionEditor } from './McqQuestionEditor';
+export type { McqQuestionFormData, McqQuestionFormErrors } from './McqQuestionEditor';
+export { McqQuestionCard } from './McqQuestionCard';
+export { McqPreview } from './McqPreview';
+export { McqImportExport } from './McqImportExport';
