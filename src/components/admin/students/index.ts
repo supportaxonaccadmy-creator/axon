776bbd9d8aa536d39498c1d4ea0b6cdf38bcaca6
supportaxonaccadmy-creator <supportaxonaccadmy@gmail.com>
2026-07-28 22:09:50 +0530

    Phase 4B.2: Student components barrel exports

---
+++ src/components/admin/students/index.ts
@@ -0,0 +1,7 @@
+export { StudentAnalytics } from './StudentAnalytics';
+export { StudentFilters } from './StudentFilters';
+export { StudentCard } from './StudentCard';
+export { StudentTable } from './StudentTable';
+export { EnrollmentTable } from './EnrollmentTable';
+export { PurchaseTable } from './PurchaseTable';
+export { EnrollmentStatusBadge, PurchaseStatusBadge, ProgressCard } from './StatusBadges';
