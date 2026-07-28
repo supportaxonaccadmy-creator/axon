9c6148cd40a89a16a50fb24c3b60032218aae19a
supportaxonaccadmy-creator <supportaxonaccadmy@gmail.com>
2026-07-28 22:36:38 +0530

    Phase 4B.4: Settings components barrel exports

---
+++ src/components/admin/settings/index.ts
@@ -0,0 +1,3 @@
+export { SettingCard, ToggleSetting, SectionHeader, StatusBadge } from './SettingComponents';
+export { PermissionMatrix } from './PermissionMatrix';
+export { SettingsSidebar } from './SettingsSidebar';
